59f9eaff9c1acbd0f4863c483fdef7f227c5f5eb
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-05-15 10:23:38 -0700

    Unit 1 project: can loop through object and append specified num of photos to the dom

--- app.js
+++ app.js
@@ -2,26 +2,31 @@ $(() => {
   //start after
   //lets call the pexels api
 
-  const key = "563492ad6f9170000100000127c6c722c0654acb97540fefc7b78d86";
+  const getImages = (e) => {
+    const key = "563492ad6f9170000100000127c6c722c0654acb97540fefc7b78d86";
 
-  $.ajax({
-    method: "GET",
-    beforeSend: function (auth) {
-      auth.setRequestHeader("Authorization", key);
-    },
-    url: `https://api.pexels.com/v1/search?query=people smiling&per_page=1&page=1`,
-  }).then(
-    (images) => {
-      console.log(images);
+    $.ajax({
+      method: "GET",
+      beforeSend: function (auth) {
+        auth.setRequestHeader("Authorization", key);
+      },
+      url: `https://api.pexels.com/v1/search?query=people smiling&per_page=8&page=1`,
+    }).then(
+      (images) => {
+        for (let i = 0; i < 5; i++) {
+          const $image = $("<img>")
+            .attr("src", images.photos[i].src.original)
+            .appendTo($("body"));
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
 
-      const $image = $("<img>")
-        .attr("src", images.photos[0].src.original)
-        .appendTo($("body"));
-    },
-    (error) => {
-      console.log(error);
-    }
-  );
+  // getImages();
 
   //end before
 });
+// Object.entries(images)[3][1][3].src.original
