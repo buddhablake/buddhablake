07f85fc1fe9c9d6014be230d2e6294e4da18dacb
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-05-15 12:03:01 -0700

    Unit 1 project:added search functionality. along with image, a link back to image now gets printed to dom upon form submit

--- app.js
+++ app.js
@@ -3,20 +3,33 @@ $(() => {
   //lets call the pexels api
 
   const getImages = (e) => {
+    e.preventDefault();
     const key = "563492ad6f9170000100000127c6c722c0654acb97540fefc7b78d86";
+    const userQuery = $("input").val();
 
     $.ajax({
       method: "GET",
       beforeSend: function (auth) {
         auth.setRequestHeader("Authorization", key);
       },
-      url: `https://api.pexels.com/v1/search?query=people smiling&per_page=8&page=1`,
+      url: `https://api.pexels.com/v1/search?query=${userQuery}&per_page=1&page=1`,
     }).then(
       (images) => {
         for (let i = 0; i < 5; i++) {
+          //creates and appends image(s)
           const $image = $("<img>")
             .attr("src", images.photos[i].src.original)
             .appendTo($("body"));
+
+          //Creates imgLink/imgTag buttons on hover
+          //imgLink
+          const $imgLink = $("<a>")
+            .attr("href", images.photos[i].src.original)
+            .appendTo($("body"));
+
+          const $imgLinkIcon = $("<ion-icon>")
+            .attr("name", "link-outline")
+            .appendTo($imgLink);
         }
       },
       (error) => {
@@ -24,6 +37,12 @@ $(() => {
       }
     );
   };
+
+  //Handles the copying of either the img tag or the link to the user clipboard. Heavily inspired by this blog post: https://www.isquaretechnologies.com/jquery-click-copy-clipboard/. The name of my function is the ssame as articles as it is the most semanctic name for the function.
+
+  const copyToClipboard = (e, element) => {};
+
+  $("form").on("submit", getImages);
 
   // getImages();
 
