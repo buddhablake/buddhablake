317eb347166c7f2c280955f0723bb065d04d0556
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-05-15 13:10:28 -0700

    Unit 1 project: added imgTag icon, clicking icon now copies the img tag to user clipboard

--- app.js
+++ app.js
@@ -15,21 +15,52 @@ $(() => {
       url: `https://api.pexels.com/v1/search?query=${userQuery}&per_page=1&page=1`,
     }).then(
       (images) => {
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 1; i++) {
           //creates and appends image(s)
+          const $imageContainer = $("<div>")
+            .addClass("image-container")
+            .appendTo($(".images-container"));
+
+          //creates the image
           const $image = $("<img>")
             .attr("src", images.photos[i].src.original)
-            .appendTo($("body"));
+            .appendTo($imageContainer);
+
+          //creates imgOptions div to store user options
+          const $imgOptions = $("<div>")
+            .addClass("img-options")
+            .appendTo($imageContainer);
 
           //Creates imgLink/imgTag buttons on hover
           //imgLink
           const $imgLink = $("<a>")
             .attr("href", images.photos[i].src.original)
-            .appendTo($("body"));
+            .appendTo($imgOptions);
 
+          //creates the imgLink icon
           const $imgLinkIcon = $("<ion-icon>")
             .attr("name", "link-outline")
             .appendTo($imgLink);
+
+          //imgTag
+          const $imgTag = $("<div>")
+            .html(`<img src="${images.photos[i].src.original}">`)
+            .attr("id", `photo${i}`)
+            .hide()
+            .appendTo($imgOptions);
+
+          //creates the imgTag Icon
+          const $imgTagIcon = $("<ion-icon>")
+            .attr("name", "code-slash-outline")
+            .appendTo($imgOptions)
+            .on("click", () => {
+              let $tempForCopy = $("<input>").appendTo($("body"));
+              $tempForCopy.val($imgTag.html()).select();
+              document.execCommand("copy");
+              $tempForCopy.remove();
+            });
+
+          console.log($imgTag.html());
         }
       },
       (error) => {
@@ -39,8 +70,6 @@ $(() => {
   };
 
   //Handles the copying of either the img tag or the link to the user clipboard. Heavily inspired by this blog post: https://www.isquaretechnologies.com/jquery-click-copy-clipboard/. The name of my function is the ssame as articles as it is the most semanctic name for the function.
-
-  const copyToClipboard = (e, element) => {};
 
   $("form").on("submit", getImages);
 
